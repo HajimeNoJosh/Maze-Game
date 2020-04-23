230cf90074ef2c8695f574a860038390f50d3487
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-23 14:24:08 -0400

    stretching the canvas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const width = 600;
-const height = 600;
+const width = window.innerWidth;
+const height = window.innerHeight;
 const borderSize = 2;
 const wallSize = 5;
 const cells = 7;
