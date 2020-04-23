1c3a309d3cec7aa19d0dc0c1b47677f73c7d1c41
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-04-23 13:22:29 -0400

    drawing horizontal segments

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const height = 600;
 const borderSize = 40;
 const cells = 3;
 
+const unitLength = width / cells;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -110,12 +112,26 @@ const stepThroughCell = (row, column) => {
     } else if (direction === "down") {
       horizontals[row][column] = true;
     }
-
+    // Visit that next cell
     stepThroughCell(nextRow, nextColumn);
   }
-
-  // Visit that next cell
 };
 
 stepThroughCell(startRow, startColumn);
-console.log(grid);
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      10,
+      { isStatic: true }
+    );
+    World.add(world, wall);
+  });
+});
